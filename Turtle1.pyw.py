d394506de4694388fd60a27ece651ffdf4032943
JonghakJung <42352521+Seedplant@users.noreply.github.com>
2019-04-18 14:52:23 +0900

    Add files via upload

---
+++ Turtle1.pyw.py
@@ -0,0 +1,36 @@
+import random
+import turtle
+
+## 함수 선언 부분 ##
+def screenLeftClick(x, y):
+    global r, g, b          ##전역변수에 접근하기 위해 global해줌
+    turtle.pencolor((r, g, b))
+    turtle.pendown()
+    turtle.goto(x, y)
+
+def screenRightClick(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+
+def screenMidClick(x, y):
+    global r, g, b
+    tSize = random.randrange(1, 10)
+    turtle.shapesize(tSize)
+    r = random.random()
+    g = random.random()
+    b = random.random()
+   
+## 변수 선언 부분 ##
+pSize = 10
+r, g, b = 0.0, 0.0, 0.0    #전역변수임
+
+## 메인 코드 부분 ##
+turtle.title('거북이로 그림 그리기')
+turtle.shape('turtle')
+turtle.pensize(pSize)
+
+turtle.onscreenclick(screenLeftClick, 1)
+turtle.onscreenclick(screenMidClick, 2)
+turtle.onscreenclick(screenRightClick, 3)
+
+turtle.done()
